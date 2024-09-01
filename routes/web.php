5e17e8892263180011577ae1935fcc8da463fcf0
Toshiyuki Abe <abechinoid@gmail.com>
2024-09-01 15:05:03 +0000

    google認証追加。

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginWithGoogleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('auth/login', function () {
+    return view('auth/login');
+});
+
+Route::get("auth/google", [
+  LoginWithGoogleController::class,
+  "redirectToGoogle",
+]);
+
+Route::get("auth/google/callback", [
+  LoginWithGoogleController::class,
+  "handleGoogleCallback",
+]);
